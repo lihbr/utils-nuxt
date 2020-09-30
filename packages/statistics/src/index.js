d11a2e259d7e84b7ee3540033601a0a078a97ccb
lihbr <lihbr@users.noreply.github.com>
2020-09-30 23:30:55 +0200

    feat(statistics): migrate package

--- packages/statistics/src/index.js
+++ packages/statistics/src/index.js
@@ -1,1 +1,38 @@
-console.log("Hello World")
+const defaultsDeep = require("lodash/defaultsDeep");
+
+const logger = require("./logger");
+
+const statisticsFunctions = {
+  generate() {
+    let startTime;
+    let count;
+    this.nuxt.hook("generate:routeCreated", () => {
+      if (!startTime) {
+        startTime = Date.now();
+        count = 0;
+      } else {
+        count++;
+      }
+    });
+    this.nuxt.hook("generate:done", () => {
+      const time = (Date.now() - startTime) / 1000;
+      const rps = count / time;
+      logger.info(`Generated ${count} routes in ${time} sec (${rps} r/s)`);
+    });
+  }
+};
+
+module.exports = function (moduleOptions) {
+  const options = defaultsDeep(moduleOptions, {
+    ignore: []
+  });
+
+  // Run each non-ignored statistics functions
+  for (const key in statisticsFunctions) {
+    if (!options.ignore.includes(key)) {
+      statisticsFunctions[key].call(this);
+    }
+  }
+
+  logger.success("Statistics module initialized");
+};
