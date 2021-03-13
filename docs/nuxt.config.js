b0fec67c21a26aa32753c91a9bcb90e5cb7a84ea
lihbr <lihbr@users.noreply.github.com>
2021-03-13 13:13:22 +0100

    docs(deps): update docus [skip ci]

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,19 +1,14 @@
-require("dotenv").config();
-
 import path from "path";
 import { withDocus } from "docus";
 
 export default withDocus({
-  loading: {
-    color: "#e84311"
-  },
   css: [
     path.join(__dirname, "/assets/css/main.css"),
     path.join(__dirname, "/assets/css/prism.css")
   ],
-  buildModules: [
-    ["@nuxtjs/netlify-files", { existingFilesDirectory: __dirname }]
-  ],
+  generate: {
+    fallback: true
+  },
   buildModules: [
     [
       "nuxt-ackee",
